3ce73f141ecafcef8a5ae7b49122ade7b01f9246
elayne <elacheu@gmail.com>
2023-09-27 13:39:32 -0500

    time penalty working, updated variables, minor changes to html/css

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,114 @@
 // Quiz Questions/Choices and Answers
-const questions = [
+var questions = [
     {
-    question: "Commonly used data types DO Not include:",
+    question: "Commonly used data types do NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: 3,
+    answer: "alerts",
     },
     {
     question: "The condition in a if / else statement is enclosed with ___.",
     choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: 1,
+    answer: "quotes",
     },
     {
     question: "Arrays in JavaScript can be used to store ____.",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: 3,
+    answer: "booleans",
     },
     {
     question: "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-    answer: 3,    
+    answer: "quotes",    
     },
     {
     question: "A very useful tool used during development and debuggings for printing to the debugger is:",
     choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    answer: 3,
+    answer: "for loops",
     }
 
 ];
 
-console.log(questions);
-
 // add start quiz button that starts timer and presents question
 
 // starting point of quiz
-let questionIndex = 0;
-let score = 0;
-let timeLeft = 75;
+var questionIndex = 0;
+var score = 0;
 
 // variables for functions
-const questionEl = document.querySelector("#questionDiv");
-const choiceList = document.querySelector("#choices")
-const timerDisplay = document.querySelector("#time-remaining");
-const scoreDisplay = document.querySelector("#score");
-const wrapper = document.querySelector(".wrapper")
+var questionsDiv = document.querySelector("#questionsDiv");
+var choiceUl = document.querySelector("#choicesUl")
+var timer = document.querySelector("#startTime");
+var timeRemaining = document.querySelector("#time-remaining");
 
 // timer variables and function
-let timerValue = 75;
+var timerLeft = 75;
 var holdInterval = 0;
 var penalty = 10;
 var ulCreate = document.createElement("ul");
 
-timerDisplay.addEventListener("click", function () {
+timer.addEventListener("click", function () {
     if (holdInterval === 0)
         holdInterval = setInterval(function () {
-            secondsLeft --;
-            timerValue.textContent = "Time " + secondsLeft;
+            timerLeft --;
+            timeRemaining.textContent = "Remaining Time: " + timerLeft + " seconds";
 
-            if (secondsLeft <=0) {
+            if (timerLeft <=0) {
                 clearInterval(holdInterval);
                 allDone();
-                timerValue.textContent = "Time's up!";
+                timeRemaining.textContent = "Time's up!";
             }
         }, 1000)
     render(questionIndex);
 });
 
+// render the questions and choices
 function render(questionIndex) {
-    questionDiv.innerHTML = "";
+    questionsDiv.innerHTML = "";
     ulCreate.innerHTML = "";
 
     for (var i = 0; i < questions.length; i++);
     var currentQuestion = questions[questionIndex].question;
     var currentChoices = questions[questionIndex].choices;
     questionsDiv.textContent = currentQuestion;
+
+    currentChoices.forEach(function (newCard) {
+        var button = document.createElement("button");
+        button.textContent = newCard;
+        ulCreate.appendChild(button); 
+        var listItem = document.createElement("li");
+        listItem.textContent = newCard;
+        questionsDiv.appendChild(ulCreate);
+        
+   
+        button.addEventListener("click", (compare));
+    })
+
 }
+
+function compare(event) {
+    console.log(event);
+    var element = event.target;
+
+    if (element.matches("button")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct!";
+        } else {
+            timerLeft = timerLeft - penalty;
+            createDiv.textContent = "Wrong!";
+        }
+
+    }
+}
+
+
+
+
+
 
 
 // add time reduction from false answer
@@ -81,7 +116,7 @@ function render(questionIndex) {
 
 
 // scorecard / save initials
-const allDone = `All done!
+var allDone = `All done!
 Your final score is`
 // enter intials - form - submit button 
 
